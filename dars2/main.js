ec19a4964e7796013359ac319417207f924581a7
shamsiddin <devshamsiddin@gmain.com>
2024-05-02 08:17:30 +0500

    abdulla

---
+++ dars2/main.js
@@ -0,0 +1,53 @@
+// math 
+
+// console.log(15%7)
+// for(let i=0; i<10; i++){
+//     if(i%2==1){
+//         console.log(i)
+//     }
+    
+// }
+
+// console.log("2"==2)
+// // console.log("2"===2)
+// let sum =1;
+// sum+1
+
+// for(let i=1; i<=100;i++){
+//     // console.log("ining qiymati:",i)
+//     // console.log("yigindi:",sum+i, "=",sum,"+",i)
+
+//     sum *= i;
+// }
+// console.log(sum)
+
+
+// and or not 
+//  console.log("dhasdbd"&&0&&"salom"&&12)//false, "",0,null,undefined,NaN
+//  console.log(undefined??"salom")
+
+let a = 2;
+
+
+// console.log(a-5) // 5
+// console.log(a) // 10
+// console.log(--a) // 9
+// console.log(++a) // 10
+// console.log(a+55) // 65
+// console.log(a--) // 9
+// console.log(a++) // 11
+
+// console.log( 1+ (a*=2))
+// a= a*2
+// console.log("B">"A") 
+// console.log("44">"43") 
+// console.log( typeof (typeof null)) 
+// console.log(typeof {}) 
+
+// null, unde, "",0,flase, NaN
+if(NaN){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
---
+++ main.js
@@ -0,0 +1,29 @@
+//data type
+//primitiv
+// let a = null
+// console.log(a)
+
+// console.log(typeof false)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// let a = "1.4545"
+
+// let b= 10;
+
+
+// console.log(a+b+a-b) // 1019
+// console.log(a*b+a/b) // 20
+// console.log(a-b+a-b) // 18
+// console.log(a/b*a-b) // 90
+// console.log(parseFloat(a))
+// let a = true
+let a = 12
+let b = "12"
+// console.log(parseFloat(b));
+// console.log(parseInt(b));
+// console.log(Number(b))
+console.log(12*"bhadcbhs")
+
+
